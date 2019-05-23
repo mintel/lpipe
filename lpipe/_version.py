0ed0186c458b7154a5737a9dc041dc2ed208b5a7
Charlie <charlie@charliemathews.com>
2019-05-23 13:34:43 -0500

    Bump version: 0.0.1 -> 0.0.2

--- lpipe/_version.py
+++ lpipe/_version.py
@@ -1,5 +1,5 @@
 __title__ = "lpipe"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __summary__ = "Lambda toolkit and message pipeline."
 __author__ = "Mintel"
 __author_email__ = "everest@mintel.com"
