234f06a20aecfe40d411562a6cc26c519340922c
Charlie <charlie@charliemathews.com>
2019-08-21 10:52:59 -0500

    Add accidentally removed line to get logger
    Former-commit-id: 79d8cd664ac76401004b4eb74110b334795edf0a

--- lpipe/sqs.py
+++ lpipe/sqs.py
@@ -25,6 +25,7 @@ def mock_sqs(func):
             botocore.exceptions.NoRegionError,
         ):
             if config("MOCK_AWS", default=False):
+                log = kwargs["logger"] if "logger" in kwargs else logging.getLogger()
                 log.debug("Mocked SQS: {}()".format(func), function=f"{func}", params={"args": f"{args}", "kwargs": f"{kwargs}"})
                 return
             else:
