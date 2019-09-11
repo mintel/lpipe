314aacc06ab9a3814bcd37575f4c768a697b3362
Charlie <charlie@charliemathews.com>
2019-09-11 12:26:07 -0500

    Remove commented out test and simplify assertions
    Former-commit-id: eba140436d8d1e4bf36029e1eeb1135cafa5bcd1

--- tests/unit/test_sqs.py
+++ tests/unit/test_sqs.py
@@ -3,43 +3,12 @@ import pytest
 from lpipe.sqs import build
 
 
-# @pytest.mark.parametrize(
-#     ["message", "message_group_id", "expected"],
-#     [
-#         (
-#             {"a": "b"},
-#             None,
-#             {
-#                 "Id": "f0c7f54eccba022bfc32fbc01bd8806d91569026",
-#                 "MessageBody": '{"a": "b"}',
-#             },
-#         ),
-#         (
-#             {"a": "b"},
-#             "10",
-#             {
-#                 "Id": "f0c7f54eccba022bfc32fbc01bd8806d91569026",
-#                 "MessageGroupId": "10",
-#                 "MessageBody": '{"a": "b"}',
-#             },
-#         ),
-#     ],
-# )
-# def test_build(message, message_group_id, expected):
-#     actual = build(message, message_group_id)
-#     assert expected == actual
-
-
 class TestBuild:
     def test_build(self):
-        assert build(message_data={"foo": "bar"}) == {
-            "Id": "bc4919c6adf7168088eaea06e27a5b23f0f9f9da",
-            "MessageBody": '{"foo": "bar"}',
-        }
+        result = build(message_data={"foo": "bar"})
+        assert result["MessageBody"] == '{"foo": "bar"}'
 
     def test_build_with_group_id(self):
-        assert build(message_data={"foo": "bar"}, message_group_id=10) == {
-            "Id": "bc4919c6adf7168088eaea06e27a5b23f0f9f9da",
-            "MessageGroupId": "10",
-            "MessageBody": '{"foo": "bar"}',
-        }
+        result = build(message_data={"foo": "bar"}, message_group_id=10)
+        assert result["MessageBody"] == '{"foo": "bar"}'
+        assert result["MessageGroupId"] == "10"
