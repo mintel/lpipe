8d7e7a0bb2d4479d83bce8c8f4f6c373e945d22a
Charlie <charlie@charliemathews.com>
2019-09-24 13:14:47 -0500

    Bump version: 1.0.5 -> 1.1.0
    Former-commit-id: 7dc2a4546dda3d142e2dca1474edd8470dfd70ae

--- lpipe/_version.py
+++ lpipe/_version.py
@@ -1,5 +1,5 @@
 __title__ = "lpipe"
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 __summary__ = "Lambda toolkit and message pipeline."
 __author__ = "Mintel"
 __author_email__ = "everest@mintel.com"
