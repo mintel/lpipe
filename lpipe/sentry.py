1f1ecd2ec0adec514a9513dda7da3ad321c6460c
Charlie Mathews <charlie@charliemathews.com>
2020-03-03 03:46:02 -0600

    Simplify sentry_sdk imports more
    Former-commit-id: 13fb055fd38867204255b82d9b6adb8f9ef88572

--- lpipe/sentry.py
+++ lpipe/sentry.py
@@ -1,9 +1,8 @@
 from contextlib import contextmanager
 from functools import wraps
 
-import sentry_sdk.init
+import sentry_sdk
 from decouple import config
-from sentry_sdk import capture_exception, configure_scope, push_scope
 from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 
 
@@ -17,7 +16,7 @@ def init(dsn: str = None, context: dict = None):
         dsn = config("SENTRY_DSN")
     sentry_sdk.init(dsn=dsn, integrations=[AwsLambdaIntegration()])
     if context:
-        with configure_scope() as scope:
+        with sentry_sdk.configure_scope() as scope:
             _set_tags(scope, context)
 
 
@@ -33,7 +32,7 @@ def _env(*keys):
 def scope(context):
     # https://docs.sentry.io/enriching-error-data/scopes/?platform=python#local-scopes
     if config("SENTRY_DSN", default=None):
-        with push_scope() as scope:
+        with sentry_sdk.push_scope() as scope:
             _set_tags(scope, context)
             yield
 
@@ -52,4 +51,4 @@ def push_context(context):
 
 def capture(e):
     if config("SENTRY_DSN", default=None):
-        capture_exception(e)
+        sentry_sdk.capture_exception(e)
