bd76993e35f25e553693e1a9d890bc113cf10879
Charlie Mathews <charlie@charliemathews.com>
2020-03-10 22:53:30 -0500

    Bump version: 2.0.9 -> 2.0.10
    Former-commit-id: 8e0515da606a105af147781825476cd6c3ebcf14

--- lpipe/_version.py
+++ lpipe/_version.py
@@ -1,5 +1,5 @@
 __title__ = "lpipe"
-__version__ = "2.0.9"
+__version__ = "2.0.10"
 __summary__ = "Lambda toolkit and message pipeline."
 __author__ = "Mintel"
 __author_email__ = "everest@mintel.com"
