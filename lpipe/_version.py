640f6e1749811ccaed79518a160e95204abc2709
Charlie Mathews <charlie@charliemathews.com>
2020-05-15 14:00:59 -0500

    Switch author info to myself.

--- lpipe/_version.py
+++ lpipe/_version.py
@@ -1,8 +1,8 @@
 __title__ = "lpipe"
 __version__ = "2.0.24"
 __summary__ = "Lambda toolkit and message pipeline."
-__author__ = "Mintel"
-__author_email__ = "everest@mintel.com"
+__author__ = "Charlie Mathews"
+__author_email__ = "cmathews@mintel.com"
 __maintainer__ = None
 __maintainer_email__ = None
-__uri__ = "https://gitlab.com/mintel/everest/lambdas/lambda-pipeline"
+__uri__ = "https://github.com/mintel/lpipe"
